bee11a8dc8e460a2ac0778239d1dfbc0b96beba6
Gérald Leban <gerald.leban0@gmail.com>
2024-11-03 12:21:39 +0100

    feat: add logo

---
+++ src/lib/components/logo.svelte
@@ -0,0 +1,55 @@
+<div class="inline-flex select-none items-center gap-2">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="32" height="32">
+		<rect width="256" height="256" fill="none"></rect>
+		<line
+			x1="80"
+			y1="56"
+			x2="176"
+			y2="56"
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="16"
+		></line>
+		<path
+			d="M80,56,64,215.24A8,8,0,0,0,72,224H184a8,8,0,0,0,8-8.76L176,56,133.12,17.85a8,8,0,0,0-10.24,0Z"
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="16"
+		></path>
+		<line
+			x1="128"
+			y1="88"
+			x2="128"
+			y2="112"
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="16"
+		></line>
+		<polyline
+			points="208 88 208 112 48 112 48 88"
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="16"
+		></polyline>
+		<line
+			x1="68.77"
+			y1="168"
+			x2="187.23"
+			y2="168"
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="16"
+		></line>
+	</svg>
+	<span class="text-2xl font-bold">Alexendria</span>
+</div>
