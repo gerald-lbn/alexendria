57dc52f033c63e7157aafe083758813d17d69859
Gérald Leban <gerald.leban0@gmail.com>
2024-11-05 17:30:10 +0100

    fix: change import path

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,7 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
-import type { User } from '$lib/server/user';
-import type { Session } from '$lib/server/session';
+import type { User } from '$lib/server/db/types';
+import type { Session } from '$lib/server/db/types';
 
 declare global {
 	namespace App {
